41d0556046a13ad89738300c72fe1017643aa378
Richard Palacios G <apalaciosg91@gmail.com>
2019-04-17 01:27:22 -0500

    Dibujado del canvas del juego, usando javascript

---
+++ display.js
@@ -0,0 +1,32 @@
+const SCALE = 20;
+//Método auxiliar para crear elementos html dinámicamente
+function createElement(type, className) {
+    let element = document.createElement(type);
+    //className <> null
+    if (className) element.className = className;
+    return element;
+}
+
+//Dibuja el fondo del juego
+function DOMDisplay(parent, level) {
+    this.wrap = parent.appendChild(createElement('div', 'game'));
+    this.level = level;
+
+    this.drawBackground = function () {
+        let table = createElement('table', 'background');
+        table.style.width = this.level.width * SCALE + 'px'; 
+
+        this.level.grid.forEach( row => {
+            let rowElement = createElement('tr');
+            rowElement.style.height = SCALE + 'px';
+            table.appendChild(rowElement);   
+            row.forEach( type => {
+                rowElement.appendChild(createElement('td', type));
+            });
+        });
+        return table;
+    }
+
+    this.wrap.appendChild(this.drawBackground());
+
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+let plan = [
+    "                                                                                ",
+    "                                                                                ",
+    "                                                                                ",
+    "                                                                                ",
+    "                                                                                ",
+    "                                                                                ",
+    "                                                                  xxx           ",
+    "                                                   xx      xx    xx!xx          ",
+    "                                    o o        xx                x!!!x          ",
+    "                                                                 xx!xx          ",
+    "                                   xxxxx                          xvx           ",
+    "                                                                            xx  ",
+    "  xx                                      o o                                x  ",
+    "  x                     o                                                    x  ",
+    "  x                                      xxxxx                             o x  ",
+    "  x          xxxx       o                                                    x  ",
+    "  x  @       x  x                                                xxxxx       x  ",
+    "  xxxxxxxxxxxx  xxxxxxxxxxxxxxx   xxxxxxxxxxxxxxxxxxxx     xxxxxxx   xxxxxxxxx  ",
+    "                              x   x                  x     x                    ",
+    "                              x!!!x                  x!!!!!x                    ",
+    "                              x!!!x                  x!!!!!x                    ",
+    "                              xxxxx                  xxxxxxx                    ",
+    "                                                                                ",
+    "                                                                                "
+];
+let currentlevel = new Level(plan);
+let display = new DOMDisplay(document.body, currentlevel);
