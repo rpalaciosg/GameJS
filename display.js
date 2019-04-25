d3a135932915b46829a0d537c03225507c2a341e
Richard Palacios G <apalaciosg91@gmail.com>
2019-04-25 17:57:56 -0500

    Se refactoriza constructor DOMdisplay para usar prototipos de JS

--- display.js
+++ display.js
@@ -12,21 +12,21 @@ function DOMDisplay(parent, level) {
     this.wrap = parent.appendChild(createElement('div', 'game'));
     this.level = level;
 
-    this.drawBackground = function () {
-        let table = createElement('table', 'background');
-        table.style.width = this.level.width * SCALE + 'px'; 
-
-        this.level.grid.forEach( row => {
-            let rowElement = createElement('tr');
-            rowElement.style.height = SCALE + 'px';
-            table.appendChild(rowElement);   
-            row.forEach( type => {
-                rowElement.appendChild(createElement('td', type));
-            });
-        });
-        return table;
-    }
-
     this.wrap.appendChild(this.drawBackground());
 
+}
+
+DOMDisplay.prototype.drawBackground = function () {
+    let table = createElement('table', 'background');
+    table.style.width = this.level.width * SCALE + 'px'; 
+
+    this.level.grid.forEach( row => {
+        let rowElement = createElement('tr');
+        rowElement.style.height = SCALE + 'px';
+        table.appendChild(rowElement);   
+        row.forEach( type => {
+            rowElement.appendChild(createElement('td', type));
+        });
+    });
+    return table;
 }
