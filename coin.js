d6c8b7d40a59ea786065ece96a76a6f8ccd3f7ba
Richard Palacios G <apalaciosg91@gmail.com>
2019-04-25 19:22:53 -0500

    Se termina el constructor para crear y dibujar el actor Coin(moneda).

--- coin.js
+++ coin.js
@@ -1,5 +1,7 @@
-function Coin () {
-
+function Coin (initialPosition) {
+    this.basePosition = this.pos = initialPosition.plus(new Vector(0.2,0.1));
+    this.size = new Vector(0.6, 0.6);
+    this.wobble = Math.PI * 2 * Math.random();
 }
 
 Coin.prototype.type = 'coin';
--- level.js
+++ level.js
@@ -1,5 +1,5 @@
 const ACTORS = {
-
+    'o': Coin
 };
 
 function Level(plan) {
