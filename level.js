864387bb3af8be430a124905c8b0e7c919e20a2b
Richard Palacios G <apalaciosg91@gmail.com>
2019-05-26 12:12:06 -0500

    Método para mover jugador de izquierda a derecha en eje X. - metodo auxiliar para saber si ay un obstaculo. obstacleAt(). - método axuliar para saber si el jugador toca algo o hay un obstaculo. playerTouched(). - función act() para mover el jugador en el eje x o y.

--- level.js
+++ level.js
@@ -73,7 +73,24 @@ Level.prototype.obstacleAt = function (position, size) {
     }
 }
 
+Level.prototype.playerTouched = function (type) {
+    if (type === 'lava' & this.status === null) {
+        this.status = 'lost';
+        this.finishDelay = 1;
+    } else if (type === 'coin') {
+        this.actors = this.actors.filter(otherActor => otherActor !== actor);
+        if (!remainCoins(this.actors)){
+            this.status = 'won';
+            this.finishDelay = 2;
+        }
+    }
+}
+
 function validateLevel (level) {
     //Verificamos que exista el jugador
     return (level.some( row => row.indexOf('@') !== -1) && level.some( row => row.indexOf('o') !== -1));
+}
+
+function remainCoins (actors) {
+    return actors.some(actor => actor.type === 'coin');
 }
--- player.js
+++ player.js
@@ -1,3 +1,5 @@
+const playerSpeed = 6;
+
 function Player (initialPosition) {
     this.position = initialPosition.plus(new Vector(0, -0.5));
     this.size = new Vector(0.8,1.5);
@@ -6,6 +8,23 @@ function Player (initialPosition) {
 
 Player.prototype.type = 'player';
 
+Player.prototype.moveX = function (step, level, keys) {
+    this.speed.x = 0;
+    if (keys.left) this.speed.x -= playerSpeed;
+    if (keys.right) this.speed.x += playerSpeed;
+    let motion = new Vector(this.speed.x * step, 0);
+    let newPosition = this.position.plus(motion);
+
+    let obstacle = level.obstacleAt(newPosition, this.size);
+    if(obstacle) level.playerTouched(obstacle);
+    else this.position = newPosition;
+}
+
+Player.prototype.moveY = function (step, level, keys) {
+
+}
+
 Player.prototype.act = function (step, level, keys) {
-    
+    this.moveX(step, level, keys);
+    this.moveY(step, level, keys);
 }
